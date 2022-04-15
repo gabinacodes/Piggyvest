546257a5ab61050d1a78d869464f73b710e75dbe
gabinacodes <badruraidah@gmail.com>
2022-04-15 18:42:46 +0100

    header html

--- piggyvest/src/componet/Header.js
+++ piggyvest/src/componet/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import "../styles/modules/Header.scss"
+import logo from "../image/Piggyvest-logo.svg"
 
 const Header = () =>{
     return(
- <header></header>
+ <header>
+  <img src={logo} alt="logo"/>
+<div>
+<a>Save</a>
+  <a>Invest</a>
+  <a>Stories</a>
+  <a>FAQs</a>
+  <a>Login</a>
+</div>
+ <button>Create A Free Account</button>
+ </header>
     )
 }
 export default Header
