1bbf9c3d569ac2c36ae9d4f3e6b56d8f36518ac0
gabinacodes <badruraidah@gmail.com>
2022-04-20 10:50:46 +0100

    adding more files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Header from "../src/componet/Header"
+import Header from "./component/Header"
+import Main from "./component/Main"
+import "./styles/global.scss"
 function App() {
   return (
     <>
     <Header/>
+    <Main/>
     </>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import logo from "../image/Piggyvest-logo.svg"
+import Styles from "../styles/modules/header.module.scss"
+
+
+const Header = () =>{
+    return(
+ <header>
+  <img src={logo} alt="logo"/>
+<span className="nav">
+<a href="http://www.google.com">Save</a>
+  <a  href="http://www.google.com">Invest</a>
+  <a  href="http://www.google.com">Stories</a>
+  <a  href="http://www.google.com">FAQs</a>
+  <a  href="http://www.google.com">Login</a>
+</span>
+ <button className="btn">Create A Free Account</button>
+ </header>
+    )
+}
+export default Header
