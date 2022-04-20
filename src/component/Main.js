e385d5c8c7a6d707a908ee752d62ee8dfb9d01ab
gabinacodes <badruraidah@gmail.com>
2022-04-20 15:37:19 +0100

    images

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Styles from '../styles/modules/main.module.scss';
-import mark from '../image/mark.png'
-import fatima from '../image/fatima.png'
-import ellie from '../image/ellie.png'
-import ayo from '../image/ayo.svg'
-import appstore from '../image/AppStore.png'
-import googleplay from '../image/googleplay.png'
+import Images from '../image';
 
 function Main() {
   return (
@@ -13,18 +8,18 @@ function Main() {
     <div className={Styles.Div} >
     <div> 
       <div className={Styles.avatar}>
-          <img className={Styles.ellie} src={ellie}  alt="ellie" />
-          <img className={Styles.mark} src={mark}  alt="mark" />
-          <img className={Styles.ayo} src={ayo}  alt="ayo" />
-          <img className= {Styles.fatima} src={fatima}  alt="fatima" />
+          <img className={Styles.ellie} src={Images.ellie}  alt="ellie" />
+          <img className={Styles.mark} src={Images.mark}  alt="mark" />
+          <img className={Styles.ayo} src={Images.ayo}  alt="ayo" />
+          <img className= {Styles.fatima} src={Images.fatima}  alt="fatima" />
       </div>
       <div className={Styles.save} >
          <h1 className={Styles.heading}>The Best way to Save & Invest</h1> 
          <p className={Styles.goal}>PiggyVest helps over 1 million customers achieve their financial goals by helping them save and invest with ease.</p>
          <button className={Styles.btn}>Create A Free Account</button>
          <div className={Styles.store}>
-             <img src={appstore} alt="appstore"/>
-             <img src={googleplay} alt="googleplay"/>
+             <img src={Images.appstore} alt="appstore"/>
+             <img src={Images.googleplay} alt="googleplay"/>
          </div>
       </div>
       </div>
---
+++ src/image/index.js
@@ -0,0 +1,11 @@
+import mark from './mark.png'
+import fatima from './fatima.png'
+import ellie from './ellie.png'
+import ayo from './ayo.svg'
+import appstore from './AppStore.png'
+import googleplay from './googleplay.png'
+const Images ={
+    mark, fatima, ellie, ayo, appstore,googleplay
+}
+
+export default Images
